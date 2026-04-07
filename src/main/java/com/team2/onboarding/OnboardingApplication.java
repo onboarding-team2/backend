ca63f0786f99181cf4efb73a66cc20280610535d
alstjs37 <kubernetes.io@kakao.com>
2026-04-07 21:44:32 +0900

    init springBoot project

---
+++ src/main/java/com/team2/onboarding/OnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.team2.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingApplication.class, args);
+	}
+
+}
