fedc9071438f3d226e6578325cec3fbff8a0f76d
juuurr <juuurr@naver.com>
2026-06-08 19:26:39 +0900

    feat: DC대시보드 데이터 조회

--- src/main/java/com/team2/onboarding/dto/DCDashboardResponseDto.java
+++ src/main/java/com/team2/onboarding/dto/DCDashboardResponseDto.java
@@ -1,12 +1,25 @@
 package com.team2.onboarding.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class DCDashboardResponseDto {
+
+    @JsonProperty("total_balance")
+    private long totalBalance;
+
+    @JsonProperty("total_employee")
     private long totalEmployee;
+
+    @JsonProperty("default_option_not_selected")
     private long defaultOptionNotSelected;
-    private long totalContributionAmount;
+
+    @JsonProperty("this_month_contribution")
+    private long thisMonthContribution;
+
+    @JsonProperty("contribution_due_date")
+    private String contributionDueDate;
 }
