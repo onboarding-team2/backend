8d294d9dbc8fbdb5bb7949ae49116848784329d5
juuurr <juuurr@naver.com>
2026-06-10 00:49:03 +0900

    security와 충돌 해결

--- src/main/java/com/team2/onboarding/config/SecurityConfig.java
+++ src/main/java/com/team2/onboarding/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.team2.onboarding.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
